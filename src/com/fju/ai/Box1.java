570747658d9336d2c0e05db656aff9e44fce71f8
yy11y1y <amylai0831@gmail.com>
2022-04-29 14:57:22 +0800

    0429

---
+++ src/com/fju/ai/Box1.java
@@ -0,0 +1,7 @@
+package com.fju.ai;
+
+public class Box1 extends Box{
+    public Box1() {
+        super(10,10,10);
+    }
+}
--- src/com/fju/ai/BoxTester.java
+++ src/com/fju/ai/BoxTester.java
@@ -21,7 +21,6 @@ package com.fju.ai;
     Please enter object's height: 21
     三號箱
  */
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
